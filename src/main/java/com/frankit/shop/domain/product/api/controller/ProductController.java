795135a3b011744ef07ab30d80f99414e380bd6f
bhkim66 <bhmklnj63@gmail.com>
2025-03-08 19:25:22 +0900

    data.sql 추가

--- src/main/java/com/frankit/shop/domain/product/api/controller/ProductController.java
+++ src/main/java/com/frankit/shop/domain/product/api/controller/ProductController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class ProductController {
 
     @Operation(summary = "여러개의 상품을 페이징 조회힌다", description = "상품 목록 조회")
     @GetMapping
-    public ResponseEntity<ApiResponseResult<Page<ProductResponse>>> selectProducts(Pageable page) {
+    public ResponseEntity<ApiResponseResult<Page<ProductResponse>>> selectProducts(@PageableDefault(page = 0, size = 10) Pageable page) {
         return ResponseEntity.ok(ApiResponseResult.success(productService.selectProducts(page)));
     }
 
